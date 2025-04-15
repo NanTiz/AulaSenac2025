832d908e99bc97c15e3f166449d5aca7698f8b97
RONAN RODRIGO DA SILVA <ronan.silva@alunos.sc.senac.br>
2025-04-14 21:00:00 -0300

    Números ímpares

--- 01/src/Exercicio04.java
+++ 01/src/Exercicio04.java
@@ -1,7 +1,7 @@
 public class Exercicio04 {
     public static void main(String[] args) {
 
-     for (int i = 1; i <= 10; i++){
+     for (int i = 0; i <= 10; i++){
          if (i %2 ==0){
              System.out.println(" Numeros pares " + i);
          }
---
+++ 01/src/Exercicio05.java
@@ -0,0 +1,10 @@
+public class Exercicio05 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            if (i %2 == 1) {
+                System.out.println(" Números ímpares " + i);
+            }
+        }
+        }
+        }
