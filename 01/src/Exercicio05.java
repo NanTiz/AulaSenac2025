3040f70245a6d3110e374a29650e7d49b8ac2859
RONAN RODRIGO DA SILVA <ronan.silva@alunos.sc.senac.br>
2025-04-14 21:32:55 -0300

    Multiplicação de N

--- 01/src/Exercicio05.java
+++ 01/src/Exercicio05.java
@@ -1,10 +1,7 @@
 public class Exercicio05 {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++) {
-            if (i %2 == 1) {
-                System.out.println(" Números ímpares " + i);
-            }
-        }
+
+
         }
         }
---
+++ 01/src/Exercicio06.java
@@ -0,0 +1,10 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+
+        int N = 1;
+        for (int i = 5; i >= 1; i--) {
+            N *= i;
+            System.out.println(" Multiplicação do N " + N);
+        }
+    }
+}
