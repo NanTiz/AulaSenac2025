873b34d64d43186f2e3b65cac94139f6e446f5d7
RONAN RODRIGO DA SILVA <ronan.silva@alunos.sc.senac.br>
2025-04-16 19:41:05 -0300

    Contagem regressiva

---
+++ 01/src/Exercicio10.java
@@ -0,0 +1,9 @@
+public class Exercicio10 {
+    public static void main(String[] args) {
+
+                int N = 10;
+                for (int i = N; i >= 0; i--){
+                    System.out.println(i);
+                }
+            }
+        }
