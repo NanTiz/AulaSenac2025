0913f0183e866696bd4510741f9d4fb900e61b0f
RONAN RODRIGO DA SILVA <ronan.silva@alunos.sc.senac.br>
2025-04-16 20:48:24 -0300

    Números pares

---
+++ 01/src/Exercicio14.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Exercicio14 {
+    public static void main(String[] args) {
+
+                ArrayList<Integer> N = new ArrayList<>();
+                N.add(1);
+                N.add(2);
+                N.add(3);
+                N.add(4);
+                N.add(5);
+                N.add(6);
+                N.add(7);
+                N.add(8);
+                N.add(9);
+                N.add(10);
+
+                int numerosPares = 0;
+
+                for (int numero : N) {
+                    if (numero % 2 == 0) {
+                        numerosPares++;
+                    }
+                }
+
+                System.out.println(" Números pares: " + numerosPares);
+            }
+        }
