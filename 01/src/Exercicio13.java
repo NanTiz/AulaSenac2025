0f03e76295caddd4a10f484d1624b0b9833e360e
RONAN RODRIGO DA SILVA <ronan.silva@alunos.sc.senac.br>
2025-04-16 20:44:34 -0300

    Maior e menor número

---
+++ 01/src/Exercicio13.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class Exercicio13 {
+    public static void main(String[] args) {
+
+
+                ArrayList<Integer> N = new ArrayList<>();
+                N.add(1);
+                N.add(2);
+                N.add(3);
+                N.add(4);
+                N.add(5);
+                N.add(6);
+                N.add(7);
+                N.add(8);
+                N.add(9);
+                N.add(10);
+
+                int maior = Collections.max(N);
+                int menor = Collections.min(N);
+
+                System.out.println(" Maior número: " + maior);
+                System.out.println(" Menor número: " + menor);
+            }
+        }
